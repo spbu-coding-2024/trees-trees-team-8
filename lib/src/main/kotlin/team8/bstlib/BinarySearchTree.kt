b4b9411b5be5d67b7b6e4ace5c4bf17a16267d3d
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-23 19:37:04 +0300

    feat: add BinarySearchTree class

---
+++ lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
@@ -0,0 +1,12 @@
+package team8.bstlib
+
+sealed class BinarySearchTree<K : Comparable<K>, V, N : BinarySearchTreeNode<K, V, N>>(rootKey: K, rootValue: V) {
+    protected abstract val root: N
+
+    val keys: MutableList<K> = mutableListOf(rootKey)
+    val values: MutableList<V> = mutableListOf(rootValue)
+
+    abstract fun insert(key: K, value: V): N?
+    abstract fun remove(key: K, value: V): N?
+    abstract fun find(key: K, value: V): N?
+}
