bdc790ce8ba37bef5a4d7e58cc9ae3e0d65d70c0
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-29 17:47:16 +0300

    feat: add rotateLeft private function

--- lib/src/main/kotlin/team8/bstlib/RBTree.kt
+++ lib/src/main/kotlin/team8/bstlib/RBTree.kt
@@ -4,6 +4,23 @@ class RBTree<K : Comparable<K>, V>(rootKey: K, rootValue: V) :
     BinarySearchTree<K, V, RBTreeNode<K, V>>(rootKey, rootValue) {
     override var root: RBTreeNode<K, V>? = RBTreeNode(Color.BLACK, rootKey, rootValue)
 
+    private fun rotateLeft(node: RBTreeNode<K, V>) {
+        val substitutionNode = node.rightChild ?: return
+        substitutionNode.parent = node.parent
+        if (node.parent?.leftChild == node)
+            node.parent?.leftChild = substitutionNode
+        else
+            node.parent?.rightChild = substitutionNode
+
+        node.rightChild = substitutionNode.leftChild
+        substitutionNode.leftChild?.parent = node
+
+        node.parent = substitutionNode
+        substitutionNode.leftChild = node
+
+        return
+    }
+
     override fun insert(key: K, value: V): RBTreeNode<K, V>? = TODO()
     override fun remove(key: K): RBTreeNode<K, V>? = TODO()
 }
