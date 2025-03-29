7ecfac157d420c85daa0e2a8e29f8d2b33660787
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-29 17:25:35 +0300

    refactor: make root mutable and nullable

--- lib/src/main/kotlin/team8/bstlib/RBTree.kt
+++ lib/src/main/kotlin/team8/bstlib/RBTree.kt
@@ -2,7 +2,7 @@ package team8.bstlib
 
 class RBTree<K : Comparable<K>, V>(rootKey: K, rootValue: V) :
     BinarySearchTree<K, V, RBTreeNode<K, V>>(rootKey, rootValue) {
-    override val root: RBTreeNode<K, V> = RBTreeNode(Color.BLACK, rootKey, rootValue)
+    override var root: RBTreeNode<K, V>? = RBTreeNode(Color.BLACK, rootKey, rootValue)
 
     override fun insert(key: K, value: V): RBTreeNode<K, V>? = TODO()
     override fun remove(key: K): RBTreeNode<K, V>? = TODO()
