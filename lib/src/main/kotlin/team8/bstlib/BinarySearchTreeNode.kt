ef6362e1d30e7d316656c0220f2361f6be490a40
Lev Beyzer <opo.rshr3@gmail.com>
2025-03-30 13:11:08 +0300

    feat(bst): add getMinimum and getMaximum methods

--- lib/src/main/kotlin/team8/bstlib/BinarySearchTreeNode.kt
+++ lib/src/main/kotlin/team8/bstlib/BinarySearchTreeNode.kt
@@ -4,4 +4,12 @@ sealed class BinarySearchTreeNode<K : Comparable<K>, V, N : BinarySearchTreeNode
     var leftChild: N? = null
     var rightChild: N? = null
     var parent: N? = null
+
+    fun getMinimum(): N {
+        return if (leftChild == null) this as N else leftChild!!.getMinimum()
+    }
+
+    fun getMaximum(): N {
+        return if (rightChild == null) this as N else rightChild!!.getMaximum()
+    }
 }
