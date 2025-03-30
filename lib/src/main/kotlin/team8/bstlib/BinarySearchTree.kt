f2d878d249015f25c63bb8b166c0ac5ab7af8f1d
Lev Beyzer <opo.rshr3@gmail.com>
2025-03-30 18:41:11 +0300

    fix: make root mutable and correct find traversal

--- lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
+++ lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
@@ -1,10 +1,13 @@
 package team8.bstlib
 
 sealed class BinarySearchTree<K : Comparable<K>, V, N : BinarySearchTreeNode<K, V, N>>(rootKey: K, rootValue: V) {
-    protected abstract val root: N?
+    protected abstract var root: N?
 
     val keys: MutableList<K> = mutableListOf(rootKey)
+        private set
+
     val values: MutableList<V> = mutableListOf(rootValue)
+        private set
 
     abstract fun insert(key: K, value: V): N?
     abstract fun remove(key: K): N?
@@ -12,10 +15,9 @@ sealed class BinarySearchTree<K : Comparable<K>, V, N : BinarySearchTreeNode<K, 
         fun recursiveFind(currentNode: N?): N? {
             if (currentNode == null || currentNode.key == key) return currentNode
             return if (currentNode.key < key)
-                recursiveFind(currentNode.leftChild)
-            else
                 recursiveFind(currentNode.rightChild)
-        }
+            else
+                recursiveFind(currentNode.leftChild)        }
         return recursiveFind(root)
     }
 }
