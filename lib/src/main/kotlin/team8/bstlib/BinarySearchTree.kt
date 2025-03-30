4a5636515257c7c3dda7d0e9806c1bf67eb23c9c
Lev Beyzer <opo.rshr3@gmail.com>
2025-03-30 20:12:38 +0300

    refactor: remove private setter from keys and values

--- lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
+++ lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
@@ -4,10 +4,7 @@ sealed class BinarySearchTree<K : Comparable<K>, V, N : BinarySearchTreeNode<K, 
     protected abstract var root: N?
 
     val keys: MutableList<K> = mutableListOf(rootKey)
-        private set
-
     val values: MutableList<V> = mutableListOf(rootValue)
-        private set
 
     abstract fun insert(key: K, value: V): N?
     abstract fun remove(key: K): N?
