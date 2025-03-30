e964b90bd25e189480715983ebba83cb37d976e6
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-30 20:48:36 +0300

    feat: add rotation functions

--- lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
+++ lib/src/main/kotlin/team8/bstlib/BinarySearchTree.kt
@@ -6,6 +6,38 @@ sealed class BinarySearchTree<K : Comparable<K>, V, N : BinarySearchTreeNode<K, 
     val keys: MutableList<K> = mutableListOf(rootKey)
     val values: MutableList<V> = mutableListOf(rootValue)
 
+    protected fun rotateLeft(node: N?) {
+        if (node == null) return
+        val substitutionNode = node.rightChild ?: return
+        substitutionNode.parent = node.parent
+        if (node.parent?.leftChild == node)
+            node.parent?.leftChild = substitutionNode
+        else
+            node.parent?.rightChild = substitutionNode
+
+        node.rightChild = substitutionNode.leftChild
+        substitutionNode.leftChild?.parent = node
+
+        node.parent = substitutionNode
+        substitutionNode.leftChild = node
+    }
+
+    protected fun rotateRight(node: N?) {
+        if (node == null) return
+        val substitutionNode = node.leftChild ?: return
+        substitutionNode.parent = node.parent
+        if (node.parent?.leftChild == node)
+            node.parent?.leftChild = substitutionNode
+        else
+            node.parent?.rightChild = substitutionNode
+
+        node.leftChild = substitutionNode.rightChild
+        substitutionNode.rightChild?.parent = node
+
+        node.parent = substitutionNode
+        substitutionNode.rightChild = node
+    }
+
     abstract fun insert(key: K, value: V): N?
     abstract fun remove(key: K): N?
     fun find(key: K): N? {
