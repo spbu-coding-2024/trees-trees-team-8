32b0aad758cd7a2874b46ceb8d8e9b2242edc5f9
Lev Beyzer <opo.rshr3@gmail.com>
2025-03-31 01:04:20 +0300

    feat(AVLTree): implement insertion of elements

--- lib/src/main/kotlin/team8/bstlib/AVLTree.kt
+++ lib/src/main/kotlin/team8/bstlib/AVLTree.kt
@@ -15,12 +15,53 @@ class AVLTree<K : Comparable<K>, V>(rootKey: K, rootValue: V) : BinarySearchTree
         return root
     }
 
-    //Удаление элемента по ключу
+    // Удаление элемента по ключу
     override fun remove(key: K): AVLNode<K, V>? {
         val node = find(key) ?: return null
         root = removeRecursive(root, key)
         keys.remove(key)
         values.remove(node.value)
         return node
+    }
+
+    // Рекурсивное добавление
+    private fun insertRecursive(
+        node: AVLNode<K, V>?, key: K, value: V): AVLNode<K, V> {
+        // Создаём новый узел, если дошли до места вставки
+        if (node == null) return AVLNode(key, value)
+
+        // Выбираем левое или правое поддерево для вставки
+        if (key < node.key) {
+            node.leftChild = insertRecursive(node.leftChild, key, value)
+            node.leftChild?.parent = node
+        } else {
+            node.rightChild = insertRecursive(node.rightChild, key, value)
+            node.rightChild?.parent = node
+        }
+
+        // Обновляем высоту и балансируем
+        node.updateHeight()
+        return balance(node)
+    }
+
+    // Рекурсивное удаление
+    private fun removeRecursive(node: AVLNode<K, V>?, key: K): AVLNode<K, V>? {
+        if (node == null) return null
+
+        when {
+            key < node.key -> {
+                node.leftChild = removeRecursive(node.leftChild, key)
+                node.leftChild?.parent = node
+            }
+            key > node.key -> {
+                node.rightChild = removeRecursive(node.rightChild, key)
+                node.rightChild?.parent = node
+            }
+            else -> return handleNodeDeletion(node) // Нашли удаляемый узел
+        }
+
+        // Обновляем высоту и балансируем
+        node.updateHeight()
+        return balance(node)
     }
 }
