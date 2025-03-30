d8306c02954764fe9bd1e7009b4871c0cadb8ba7
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-30 21:35:38 +0300

    refactor: delete rotation functions from RBTree

--- lib/src/main/kotlin/team8/bstlib/RBTree.kt
+++ lib/src/main/kotlin/team8/bstlib/RBTree.kt
@@ -4,42 +4,6 @@ class RBTree<K : Comparable<K>, V>(rootKey: K, rootValue: V) :
     BinarySearchTree<K, V, RBTreeNode<K, V>>(rootKey, rootValue) {
     override var root: RBTreeNode<K, V>? = RBTreeNode(Color.BLACK, rootKey, rootValue)
 
-    private fun rotateLeft(node: RBTreeNode<K, V>?) {
-        if (node == null) return
-        val substitutionNode = node.rightChild ?: return
-        substitutionNode.parent = node.parent
-        if (node.parent?.leftChild == node)
-            node.parent?.leftChild = substitutionNode
-        else
-            node.parent?.rightChild = substitutionNode
-
-        node.rightChild = substitutionNode.leftChild
-        substitutionNode.leftChild?.parent = node
-
-        node.parent = substitutionNode
-        substitutionNode.leftChild = node
-
-        return
-    }
-
-    private fun rotateRight(node: RBTreeNode<K, V>?) {
-        if (node == null) return
-        val substitutionNode = node.leftChild ?: return
-        substitutionNode.parent = node.parent
-        if (node.parent?.leftChild == node)
-            node.parent?.leftChild = substitutionNode
-        else
-            node.parent?.rightChild = substitutionNode
-
-        node.leftChild = substitutionNode.rightChild
-        substitutionNode.rightChild?.parent = node
-
-        node.parent = substitutionNode
-        substitutionNode.rightChild = node
-
-        return
-    }
-
     private fun balanceInsertion(insertedNode: RBTreeNode<K, V>?) {
         if (insertedNode == null) return
         var node = insertedNode
