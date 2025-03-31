10e89a4ecf6f9f50e0cc943a478f66a7f783935e
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-31 20:26:34 +0300

    feat: implement node equals method

--- lib/src/main/kotlin/team8/bstlib/RBTreeNode.kt
+++ lib/src/main/kotlin/team8/bstlib/RBTreeNode.kt
@@ -1,4 +1,15 @@
 package team8.bstlib
 
 class RBTreeNode<K : Comparable<K>, V>(var color: Color, key: K, value: V) :
-    BinarySearchTreeNode<K, V, RBTreeNode<K, V>>(key, value)
+    BinarySearchTreeNode<K, V, RBTreeNode<K, V>>(key, value) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is RBTreeNode<*, *>) return false
+        return this.color == other.color &&
+                this.key == other.key &&
+                this.value == this.value &&
+                this.parent == this.parent &&
+                this.leftChild == this.leftChild &&
+                this.rightChild == this.rightChild
+    }
+}
