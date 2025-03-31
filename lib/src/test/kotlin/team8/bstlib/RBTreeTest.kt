ecca832da9e9f85b50b4031089c1e438aca11033
Mikhail Ivanov <183078283+ivanovm-main@users.noreply.github.com>
2025-03-31 21:22:58 +0300

    test: add 2 basic cases

--- lib/src/test/kotlin/team8/bstlib/RBTreeTest.kt
+++ lib/src/test/kotlin/team8/bstlib/RBTreeTest.kt
@@ -74,4 +74,39 @@ class RBTreeTest {
         assert(expectedResult == actualResult)
     }
 
+    //basic cases
+    @Test
+    @DisplayName("insert when parent is black")
+    fun insertWhenParentIsBlack() {
+        val testTree = RBTree(1, 2)
+        testTree.insert(2, 2)
+        testTree.find(2)?.color = Color.BLACK
+        testTree.insert(0, 2)
+        testTree.find(0)?.color = Color.BLACK
+        val expectedResult = RBTreeNode(Color.RED, 3, 2)
+        expectedResult.parent = testTree.find(1)
+        val actualResult = testTree.insert(3, 2)
+
+        assert(testTree.find(2)?.color == Color.BLACK)
+        assert(testTree.find(0)?.color == Color.BLACK)
+        assert(expectedResult == actualResult)
+    }
+
+    @Test
+    @DisplayName("insert when parent is red")
+    fun insertWhenParentIsRed() {
+        val testTree = RBTree(1, 2)
+        testTree.insert(2, 2)
+        testTree.insert(0, 2)
+
+        val expectedResult = RBTreeNode(Color.RED, 3, 2)
+        expectedResult.parent = testTree.find(1)
+        val actualResult = testTree.insert(3, 2)
+
+        assert(testTree.find(2)?.color == Color.RED)
+        assert(testTree.find(0)?.color == Color.RED)
+        assert(testTree.find(1)?.color == Color.BLACK)
+        assert(expectedResult == actualResult)
+    }
+
 }
