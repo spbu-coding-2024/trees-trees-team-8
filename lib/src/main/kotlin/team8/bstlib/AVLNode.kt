1c5819f4b33faf79a786fdb1699c6563d571e183
lev666 <opo.rshr3@gmail.com>
2025-04-06 23:30:21 +0300

    refactor: encapsulate AVLNode internal fields

--- lib/src/main/kotlin/team8/bstlib/AVLNode.kt
+++ lib/src/main/kotlin/team8/bstlib/AVLNode.kt
@@ -1,13 +1,10 @@
 package team8.bstlib
 
 internal class AVLNode<K : Comparable<K>, V>(key: K, value: V) : BinarySearchTreeNode<K, V, AVLNode<K, V>>(key, value) {
-
-    internal var nodeHeight: Int = 1
+    internal var nodeHeight = 1
 
     internal fun updateHeight() {
-        val left = leftChild?.nodeHeight ?: 0
-        val right = rightChild?.nodeHeight ?: 0
-        nodeHeight = maxOf(left, right) + 1
+        nodeHeight = maxOf(leftChild?.nodeHeight ?: 0, rightChild?.nodeHeight ?: 0) + 1
     }
 
     internal fun balanceFactor() = (leftChild?.nodeHeight ?: 0) - (rightChild?.nodeHeight ?: 0)
